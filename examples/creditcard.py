9d1c58b9f99816502ccb20abd5a725c9839ed1ee
IraitzM <iraitzm@gmail.com>
2023-10-01 13:32:57 +0200

    Added noise levels

--- examples/creditcard.py
+++ examples/creditcard.py
@@ -1,6 +1,7 @@
+import json
+import datasets
 import pandas as pd
 from dcm import dcm
-import datasets
 
 dataset_file = 'examples/datasets/creditcard.csv'
 datasets.download_creditcard(dataset_file)
@@ -11,8 +12,19 @@ X = dataset.drop(columns=['Class']).values
 y = dataset['Class'].values
 print("DONE")
 
+print("Calculating F1 ... ", end="", flush=True)
+ratios, F1 = dcm.F1(X, y)
+print("DONE")
+print("F1 = {}".format(F1))
+print("Discriminant Ratios = {}".format(json.dumps(ratios, indent=2)))
+
 print("Calculating C1 and C2 ... ", end="", flush=True)
 C1, C2 = dcm.C12(X, y)
 print("DONE")
 print("C1 = {}".format(C1))
 print("C2 = {}".format(C2))
+
+print("Calculating N1 ... ", end="", flush=True)
+N1 = dcm.N1(X, y)
+print("DONE")
+print("N1 = {}".format(N1))
